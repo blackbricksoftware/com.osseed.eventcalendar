f70b35f169ac596b42879325badefe55a8b819b3
David Hayes <david@blackbrick.software>
2019-12-04 04:22:35 -0800

    fix calendar display

--- js/rendereventcalendar.js
+++ js/rendereventcalendar.js
@@ -1,30 +1,45 @@
-if (typeof(jQuery) != 'function')
-  var jQuery = cj;
 
-cj(function(){
-  buildCalendar();
-});
+(function(jQueryMaybe){
 
-function buildCalendar() {
+  var cj;
 
-  var events_data = cj('#calendar').data('events');
-  var showTime = events_data.timeDisplay;
+  if (jQueryMaybe.fullCalendar) {
+    cj = jQueryMaybe;
+  } else if (jQuery.fullCalendar) {
+    cj = jQuery;
+  } else {
+    console.warn('Could not find Full Calendar');
+    return;
+  }
 
-  cj('#calendar').fullCalendar({
-    eventSources: [
-      { events: events_data.events,}
-    ],
-    displayEventTime: showTime ? 1 : 0,
-    timeFormat: 'h(:mm)A',
+  function buildCalendar() {
 
-    eventRender: function eventRender( event, element, view ) {
-      if(event.eventType && events_data.isfilter == "1" ) {
-        return ['all', event.eventType].indexOf(cj('#event_selector').val()) >= 0
+    var events_data = cj('#calendar').data('events');
+    console.log(events_data);
+    var showTime = events_data.timeDisplay;
+  
+    cj('#calendar').fullCalendar({
+      eventSources: [
+        { events: events_data.events,}
+      ],
+      displayEventTime: showTime ? 1 : 0,
+      timeFormat: 'h(:mm)A',
+  
+      eventRender: function eventRender( event, element, view ) {
+        if(event.eventType && events_data.isfilter == "1" ) {
+          return ['all', event.eventType].indexOf(cj('#event_selector').val()) >= 0
+        }
       }
-    }
+    });
+  
+    cj('#event_selector').on('change', function(){
+      cj('#calendar').fullCalendar('rerenderEvents');
+    })
+  }
+  
+  cj(function(){
+    buildCalendar();
   });
 
-  cj('#event_selector').on('change', function(){
-    cj('#calendar').fullCalendar('rerenderEvents');
-  })
-}
+})(CRM.$);
+
